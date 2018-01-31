4da8d367d378c4a7c7af4284396ab5fdaf84912a
Bill Hart <Bill.Hart@civica.co.uk>
2018-01-31 15:56:59 +0000

    Amended deploy script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,6 +5,6 @@ version=$(./utils/get-site-version.sh)
 
 docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
 docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
-docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
+# docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
 docker push quay.io/ukhomeofficedigital/$NAME:$version
-docker push quay.io/ukhomeofficedigital/$NAME:lastest
+# docker push quay.io/ukhomeofficedigital/$NAME:lastest
