a090c6dda02f05a5f4d824f24dd487a602b52634
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-05 09:59:26 +0000

    Amended build scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 NAME="${1}"
-version=$(./utils/get-site-version.sh)
-
+# version=$(./utils/get-site-version.sh)
+version=`cat ./pom.xml | grep version | tail -1 | sed 's/[^0-9,.]*//g'`
 docker build -t $NAME:$version .
