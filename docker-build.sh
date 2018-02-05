23656a0fc291a1c0eeaa68a7b69b774c69c47aaf
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-05 11:33:51 +0000

    Amended build scripts

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 NAME="${1}"
-# version=$(./utils/get-site-version.sh)
-version=`cat ./pom.xml | grep version | tail -1 | sed 's/[^0-9,.]*//g'`
+version=$(./utils/get-site-version.sh)
+
+# version=`cat ./pom.xml | grep version | tail -1 | sed 's/[^0-9,.]*//g'`
 docker build -t $NAME:$version .
--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 NAME="${1}"
-# version=$(./utils/get-site-version.sh)
-version=`cat ./pom.xml | grep version | tail -1 | sed 's/[^0-9,.]*//g'`
+version=$(./scripts/bash/get-site-version.sh)
 
 docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
 docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
 docker push quay.io/ukhomeofficedigital/$NAME:$version
 
-#docker tag $NAME:$version pipe.egarteam.co.uk/$NAME:latest
-#docker push pipe.egarteam.co.uk/$NAME:latest
+docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
+docker push quay.io/ukhomeofficedigital/$NAME:latest
